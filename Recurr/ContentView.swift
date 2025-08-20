906e115160c3e9d10c6c3e4cc9fa7711fa64fffb
z1mow <zimowcs@gmail.com>
2025-08-20 15:32:38 +0300

    Initial commit

---
+++ Recurr/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Recurr
+//
+//  Created by Şakir Yılmaz ÖĞÜT on 20.08.2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Recurr/RecurrApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecurrApp.swift
+//  Recurr
+//
+//  Created by Şakir Yılmaz ÖĞÜT on 20.08.2025.
+//
+
+import SwiftUI
+
+@main
+struct RecurrApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
